523c2dc8689c73a5bb64fcc4bd6f6bf2663d931e
ayoemman <56937302+ayoemman@users.noreply.github.com>
2024-09-17 13:23:50 -0400

    Update convert_json_to_xlsx.py

--- convert_json_to_xlsx.py
+++ convert_json_to_xlsx.py
@@ -1,20 +1,17 @@
-import json
-import sys
+#import sys
 import pandas as pd
+import json
 
 def convert_json_to_xlsx(json_file, xlsx_file):
     with open(json_file, 'r') as f:
         data = json.load(f)
-    
+    df = pd.json_normalize(data)
     writer = pd.ExcelWriter(xlsx_file, engine='xlsxwriter')
-
-    for key, value in data.items():
-        df = pd.json_normalize(value)
-        df.to_excel(writer, sheet_name=key, index=False)
-
+    df.to_excel(writer, index=False, sheet_name='Data')
     writer.save()
 
 if __name__ == "__main__":
+    import sys
     json_file = sys.argv[1]
     xlsx_file = sys.argv[2]
     convert_json_to_xlsx(json_file, xlsx_file)
