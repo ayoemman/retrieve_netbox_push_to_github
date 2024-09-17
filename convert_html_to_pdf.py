9732b9d8e528bf883b1c174e55206809e4762e80
ayoemman <56937302+ayoemman@users.noreply.github.com>
2024-09-17 15:45:08 -0400

    Update convert_html_to_pdf.py

--- convert_html_to_pdf.py
+++ convert_html_to_pdf.py
@@ -1,10 +1,11 @@
 import pdfkit
-import sys
 
 def html_to_pdf(input_html, output_pdf):
-    pdfkit.from_file(input_html, output_pdf)
+    config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')  # Update the path as necessary
+    pdfkit.from_file(input_html, output_pdf, configuration=config)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    import sys
     input_html = sys.argv[1]
     output_pdf = sys.argv[2]
     html_to_pdf(input_html, output_pdf)
