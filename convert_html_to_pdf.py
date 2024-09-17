d74b80e5dffbe85a9d7f43618ff5a87fff9a5fc5
ayoemman <56937302+ayoemman@users.noreply.github.com>
2024-09-17 14:44:40 -0400

    Update convert_html_to_pdf.py

--- convert_html_to_pdf.py
+++ convert_html_to_pdf.py
@@ -1,13 +1,10 @@
 import pdfkit
+import sys
 
-def convert_html_to_pdf(html_file, pdf_file):
-    try:
-        pdfkit.from_file(html_file, pdf_file)
-        print(f"Successfully converted {html_file} to {pdf_file}")
-    except Exception as e:
-        print(f"Failed to convert {html_file} to {pdf_file}: {e}")
+def html_to_pdf(input_html, output_pdf):
+    pdfkit.from_file(input_html, output_pdf)
 
 if __name__ == "__main__":
-    html_file = "/home/user1/netbox_data/netbox_data.html"  # Change this to your HTML file path
-    pdf_file = "/home/user1/netbox_data/netbox_data.pdf"    # Change this to your desired PDF file path
-    convert_html_to_pdf(html_file, pdf_file)
+    input_html = sys.argv[1]
+    output_pdf = sys.argv[2]
+    html_to_pdf(input_html, output_pdf)
